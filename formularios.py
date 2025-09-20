a1df800068ddfa823f881df4134ea200852b46cd
/Coder_404 <mikeykkj15@gmail.com>
2025-09-20 18:22:45 +0100

    Delete formularios.py

--- formularios.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField, SelectField, SubmitField
-from wtforms.validators import DataRequired
-
-class NoteForm(FlaskForm):
-    title = StringField("Título", validators=[DataRequired()])
-    content = TextAreaField("Conteúdo", validators=[DataRequired()])
-    category = SelectField("Categoria", validators=[DataRequired()], coerce=int)
-    submit = SubmitField("Salvar Nota")
